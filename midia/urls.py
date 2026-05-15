317defbcfe4e4a4d332ed7377e3ef84f3809689a
Rene Arthur <rene_arthur@outlook.com>
2026-05-15 00:51:48 -0300

    adicionado o resto das urls

--- midia/urls.py
+++ midia/urls.py
@@ -1,11 +1,17 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from app.views import CategoriaViewSet, MidiaViewSet
+from app.views import CategoriaViewSet, EpisodioViewSet, GroupViewSet, MidiaViewSet, PermissionViewSet, TemporadaViewSet, UserViewSet
 
 router = DefaultRouter(trailing_slash=False)
+router.register('users', UserViewSet)
+router.register('groups', GroupViewSet)
+router.register('permissions', PermissionViewSet)
+
 router.register('categorias', CategoriaViewSet)
 router.register('midias', MidiaViewSet)
+router.register('temporadas', TemporadaViewSet)
+router.register('episodios', EpisodioViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
